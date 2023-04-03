af0948b8f413b60e1eff2af61d62adb376fb797d
aw74aw74 <aw74@ya.ru>
2023-04-03 22:29:28 +0500

    Задачв 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] col)
+{
+  Console.WriteLine($"Введите массив из {col.Length} элементов.");
+  for (int index = 0; index < col.Length; index++)
+  {
+    Console.Write($"Число {index + 1} из {col.Length}: ");
+    col[index] = int.Parse(Console.ReadLine()!);
+  }
+}
+
+int[] array = new int[8];
+FillArray(array);
+Console.WriteLine($"Массив из {array.Length} элементов : [ {String.Join(", ", array)} ]");
