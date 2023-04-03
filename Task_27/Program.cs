580c30f1090d49898c56ba4a46bfd31364b425f3
aw74aw74 <aw74@ya.ru>
2023-04-03 22:06:40 +0500

    Задачв 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int NumSum(int num)
+{
+  int res = 0;
+  while (num > 0)
+  {
+    res += num % 10;
+    num = num / 10;
+  }
+  return res;
+}
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр в чиле {a} равна {NumSum(a)}.");
